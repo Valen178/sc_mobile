5a5114ed1250e8807b6782a8fbcb2364be593735
Valen178 <valentino.cortese@davinci.edu.ar>
2025-12-01 22:35:49 -0300

    Update button colors and clean up unused XML entries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,7 +51,7 @@ dependencies {
     // Google Maps
     implementation("com.google.android.gms:play-services-maps:18.2.0")
     implementation("com.google.android.gms:play-services-location:21.0.1")
-
+    
     // Base de Google Play Services (para GoogleApiAvailability)
     implementation("com.google.android.gms:play-services-base:18.3.0")
 
